0b5054737edda1e0d2965ac6cfac3bb806740bed
koooommmm <nico.pika17@gmail.com>
2023-02-03 00:02:29 +0900

    fix AddItem component to modal

--- kakeibo-react/src/components/AddItem.js
+++ kakeibo-react/src/components/AddItem.js
@@ -3,10 +3,17 @@ import { v4 as uuidv4 } from "uuid";
 
 const AddItem = (props) => {
   const setItems = props.setItems;
+  const showAddItemModal = props.showAddItemModal;
+  const setShowAddItemModal = props.setShowAddItemModal;
+
   const refDate = useRef();
   const refCategory = useRef();
   const refPrice = useRef();
   const refName = useRef();
+
+  const handleCloseModal = () => {
+    setShowAddItemModal(false);
+  };
 
   const handleAddItem = () => {
     const date = refDate.current.value;
@@ -34,22 +41,31 @@ const AddItem = (props) => {
     refCategory.current.value = null;
     refPrice.current.value = null;
     refName.current.value = null;
+
+    setShowAddItemModal(false);
   };
 
-  return (
-    <>
-      日付: <input type="date" ref={refDate} />
-      カテゴリ:
-      <select ref={refCategory}>
-        <option value="食費">食費</option>
-        <option value="公共料金">公共料金</option>
-        <option value="娯楽">娯楽</option>
-      </select>
-      金額: <input type="text" pattern="^[0-9]+$" ref={refPrice} />
-      名前: <input type="text" ref={refName} />
-      <button onClick={handleAddItem}>登録</button>
-    </>
-  );
+  if (showAddItemModal) {
+    return (
+      <div id="overlay">
+        <div id="content">
+          日付: <input type="date" ref={refDate} />
+          カテゴリ:
+          <select ref={refCategory}>
+            <option value="食費">食費</option>
+            <option value="公共料金">公共料金</option>
+            <option value="娯楽">娯楽</option>
+          </select>
+          金額: <input type="text" pattern="^[0-9]+$" ref={refPrice} />
+          名前: <input type="text" ref={refName} />
+          <button onClick={handleAddItem}>登録</button>
+          <p>
+            <button onClick={handleCloseModal}>close</button>
+          </p>
+        </div>
+      </div>
+    );
+  }
 };
 
 export default AddItem;
--- kakeibo-react/src/components/KakeiboBoard.js
+++ kakeibo-react/src/components/KakeiboBoard.js
@@ -2,14 +2,25 @@ import React from "react";
 import ItemList from "./ItemList";
 import AddItem from "./AddItem";
 import { useState } from "react";
+import "./KakeiboBoard.css";
 
 const KakeiboBoard = () => {
   const [expences, setExpenses] = useState([]);
+  const [showAddItemModal, setShowAddItemModal] = useState(false);
+
+  const handleClickAddButton = () => {
+    setShowAddItemModal(true);
+  };
 
   return (
     <>
       <ItemList kind="expence" items={expences} />
-      <AddItem setItems={setExpenses} />
+      <button onClick={handleClickAddButton}>登録</button>
+      <AddItem
+        setItems={setExpenses}
+        showAddItemModal={showAddItemModal}
+        setShowAddItemModal={setShowAddItemModal}
+      />
     </>
   );
 };
