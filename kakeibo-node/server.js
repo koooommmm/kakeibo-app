96d5c79b77e8aabdf1e79bdea22dca37308d3cd2
koooommmm <nico.pika17@gmail.com>
2023-02-03 18:19:35 +0900

    add get items API

--- kakeibo-node/server.js
+++ kakeibo-node/server.js
@@ -1,15 +1,27 @@
-'use strict';
+"use strict";
 
-const express = require('express');
+const express = require("express");
+const mysql = require("mysql2");
+
+// create the connection to database
+const connection = mysql.createConnection({
+  host: "db",
+  user: "kakeibo-user",
+  password: "password",
+  database: "kakeibo",
+});
 
 // Constants
 const PORT = 8080;
-const HOST = '0.0.0.0';
+const HOST = "0.0.0.0";
 
 // App
 const app = express();
-app.get('/', (req, res) => {
-  res.send('Hello World');
+
+app.get("/items", (req, res) => {
+  connection.execute("select * from item;", (err, results, fields) => {
+    res.send(results);
+  });
 });
 
 app.listen(PORT, HOST, () => {
